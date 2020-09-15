deac7a484aa5c9f8358afd8c19af88eb2874d9b1
luisabellan <luis.abellan.dev@gmail.com>
2020-09-15 05:28:00 +0200

    save

--- cpu.py
+++ cpu.py
@@ -1,489 +1,243 @@
 """CPU functionality."""
+import sys
 
-import sys,os
-from pathlib import Path
 
+# OP CODES
+CMP = 0b10100111
+MUL = 0b10100010
+LDI = 0b10000010
+ADD = 0b10100000
+PRN = 0b01000111
+JEQ = 0b01010101
+JNE = 0b01010110
+HLT = 0b00000001
+AND = 0b10101000
+NOT = 0b01101001
+OR = 0b10101010
+PUSH = 0b01000101
+RET = 0b00010001
+POP = 0b01000110
+CALL = 0b01010000
+JMP = 0b01010100
+XOR = 0b10101011
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        # self.cpu = [0] * 256 # 256 = 32 * 8,
-        self.pc = 0
-        self.mar = [0] * 8
-        self.mdr = [0] * 8
-        self.ir = [0] * 8
-        # self.reg = {
-        #     0 : [0],
-        #     1 : [0],
-        #     2 : [0],
-        #     3 : [0],
-        #     4 : [0],
-        #     5 : [0], # reserved as the interrupt mask (IM)
-        #     6 : [0], # reserved as the interrupt status (IS)
-        #     7 : [0]  # reserved as the stack pointer (SP)
-        # }
-        self.reg = [0] * 8
-
-        # boot
-        self.fl = [0] * 8 # 0 for false and 1 for true
-        self.mar = self.pc
-        self.reg[7] = 0xF4
-        self.ram = [0] * 2048 # 256 bytes * 8 bits/byte = 2048bits
-        self.running = True
-        # self.ccr : [0] * 8
-        # self.ie = {}
-
-
-        def trace(self):
-
-            """
-            Handy function to print out the CPU state. You might want to call this
-            from run() if you need help debugging."""
-
-            print(f"TRACE: %02X | %02X %02X %02X |" % (
-                self.pc,
-                self.fl,
-                #self.ie,
-                self.ram_read(self.pc),
-                self.ram_read(self.pc + 1),
-                self.ram_read(self.pc + 2),
-
-            ), end='')
-
-            for i in range(8):
-                print(" %02X" % self.reg[i], end='')
-
-            print()
-
-    def ram_read(self, address):
-        # print(f'address: {address}')
-
-        self.mar = address
-        self.ir = self.ram[address]
-        # print(self.ir)
-        output = self.ir
-        # print(output)
-        return output
-
-    def ram_write(self, value, address):
-        self.mdr = value
-        self.mar = address
-        self.ram[address] = value
-
-    def next_operation(self):
-        instruction = self.ram_read(self.pc)
-        self.pc += int(instruction,2) >> 5
+        self.ram = [0] * 256
+        self.reg = [0] * 8 
+        self.pc = 0 
+        self.sp = 244 # F4 = 244 
+        self.reg[7] = self.sp
+        self.running = True 
+        self.flag = [0] * 8 
+        self.operations = { 
+            HLT: self.hlt,
+            LDI: self.ldi,
+            PRN: self.prn,
+            PUSH: self.push,
+            POP: self.pop,
+            CALL: self.call,
+            RET : self.ret,
+            CMP: self.cmp,
+            JEQ: self.my_jeq,
+            JNE: self.my_jne,
+            AND: self.my_and,
+            NOT: self.my_not,
+            OR: self.my_or,
+            XOR: self.my_xor
+        }
         
-    def load(self, filename):
+    
+   
+
+    def load(self):
         """Load a program into memory."""
 
         address = 0
 
-
-
-        # this does not work on Windows because of backlashes vs forward slashes.
-        # program = open(f"../ls8/examples/{filename}", "r").readlines()
-
-        # another way: using with open
-        # with open(os.path.join(f'{sys.path[0]}/examples/', filename), 'r') as program:
-
-        # New way - using pathlib (compatible with Mac, Win and Linux):
-
-        data_folder = Path("./")
-
-        filename = data_folder / filename
-
-        program = open(filename)
-
-        out = []
-        # program =
-        for line in program:
-            if line[0] == '#' or line[0] == '\n':
-                continue
-            out.append(bin(int(line[:8],2)))
-        for instruction in out:
-
-            self.ram[address] = instruction
-            address += 1
-        # print(out)
-
-    def alu(self, op, reg_a = 0, reg_b = 0):
-        """ALU operations."""
-        # print('this is an ALU operation')
-        # print(f'reg_a: {int(reg_a,2)}')
-        # print(f'reg_b: {int(reg_b,2)}')
+        filename = sys.argv[1]
+      
+        with open(filename)as f:
+            for line in f:
+                line = line.split("#")
+                opcode = line[0].strip()
+                if opcode == "":
+                    continue
+                num = int(opcode, 2)
+                self.ram_write(num, address)
+                address += 1
+            
+    def ram_read(self, mar):
         
-        
+        return self.ram[mar]
 
-        if op == "ADD":
-            print('started ADD')
-            reg_a = int(reg_a,2)
-            reg_b = int(reg_b,2)
-            a = int(self.reg[reg_a],2)
-            b = int(self.reg[reg_b],2)
-            self.reg[int(reg_a,2)] = a + b
-            print('ADD done')
-            self.pc += 3
-
-
-        elif op == "SUB":
-            reg_a = int(reg_a,2)
-            reg_b = int(reg_b,2)
-            a = int(self.reg[reg_a],2)
-            b = int(self.reg[reg_b],2)
-            self.reg[reg_a] = a - b
-            print('SUB done')
-            self.pc += 3
-
-        elif op == "MUL":
-            reg_a = int(reg_a,2)
-            reg_b = int(reg_b,2)
-            print('MUL started')
-            a = int(self.reg[reg_a],2)
-            b = int(self.reg[reg_b],2)
-            print(f'a = {a}')
-            print(f'b = {b}')
-
-         
-            self.reg[reg_a] = a * b
-            
-            print('MUL done')
-            self.pc += 3
-
-        elif op == "JEQ":
-            print('JEQ started')
-            reg_a = int(reg_a,2)
-            if self.fl[-1] == 1:
-                self.pc = reg_a
-            self.pc += 2
-            print('JEQ done')
-
-        elif op == "JMP":
-            print('JMP started')
-            reg_a = int(reg_a,2)
-            
-            self.pc = reg_a
-            
-            print('JMP done')
-        elif op == "JNE":
-            print('JNE started')
-            
-            
-            if self.fl[-1] == 0:
-                print(f'reg_a = {int(self.reg[int(reg_a,2)],2)}')
-                address = int(self.reg[int(reg_a,2)],2)
-                self.pc = address
-                
-                print(f'self.pc = {self.pc}')   
-                
-            
-                
-
-            print('JNE done')
-
-        elif op == "DIV":
-            print('DIV started')
-            reg_a = int(reg_a,2)
-            reg_b = int(reg_b,2)
-            a = int(self.reg[reg_a],2)
-            b = int(self.reg[reg_b],2)
-            if self.reg[reg_b] == 0:
-                print("error: cannot divide by zero")
-            
-            self.reg[reg_a] = a // b
-            self.pc += 3
-            print('DIV done')
-
-        elif op == "CMP":
-            print('started CMP')
-            reg_a = int(reg_a,2)
-            reg_b = int(reg_b,2)
-            print('CMP started')
-            a = int(self.reg[reg_a],2)
-            b = int(self.reg[reg_b],2)
-            print(f'a = {a}')
-            print(f'b = {b}')
-
-
-            if a < b:
-                print(f'{a} is lesser than {b}')
-                self.fl[-3] = 1
-            else:
-                self.fl[-3] = 0
-
-
-
-            if a > b:
-                print(f'{a} is larger than {b}')
-                self.fl[-2] = 1
-
-            else:
-                self.fl[-2] = 0
-
-
-
-            if a == b:
-                print(f'{a} equals {b}')
-
-                self.fl[-1] = 1
-                
-            else:
-                self.fl[-1] = 0
-
-            print('CMP done')    
-            self.pc += 3
-            
-
-            
-        else:
-            raise Exception("Unsupported ALU operation")
-            
-        
-
-
-    def non_alu(self, op, reg_a = 0, reg_b = 0):
-        
-        
-        """non-ALU operations."""
-        # print('this is an non-ALU operation')
-        # print(f'reg_a: {reg_a}')
-        # print(f'reg_b: {reg_b}')
-        
-
-        if op == "LDI":
-            print('LDI started')
-            reg_a = int(reg_a,2)
-            
-            # print(f'reg_a:{reg_a}')
-            # print(f'reg_b:{reg_b}')
-            self.reg[reg_a] = reg_b
-            self.pc += 3
-            print('LDI done')
-        
-       
-
-
-        elif op == "PRN":
-            print('PRN started')
-            reg_a = int(reg_a,2)
-           
-
-            print(type(self.reg[reg_a]))
-            if type(self.reg[reg_a]) == str:
-                
-                print(int( self.reg[reg_a][2:],2))
-            else:
-                
-                print(self.reg[reg_a])
-            
-            self.pc += 2
-            print('PRN done')
-
-        elif op == "HLT":
-            print(int(self.ram_read(self.pc),2))
-            print('HLT started')
-            
-            self.pc += 1
-            print('HLT done')
-            self.running = False
-        
-        
-        else:
-            raise Exception("Unsupported NON-ALU operation")
-
-
-
+    def ram_write(self, mdr, mar):
+        self.ram[mar] = mdr
 
    
+    def alu(self, op, reg_a, reg_b=0):
+        """ALU operations."""
+
+        if op == ADD:
+            self.reg[reg_a] += self.reg[reg_b]
+        elif op == MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
+        
+        elif op == CMP:
+            if self.reg[reg_a] > self.reg[reg_b]:
+                self.flag = 0b00000100
+            elif self.reg[reg_a] < self.reg[reg_b]:
+                self.flag = 0b00000010
+            elif self.reg[reg_a] == self.reg[reg_b]:
+                self.flag = 0b00000001
+        # Stretch                
+        elif op == AND:
+            self.reg[reg_a] &= self.reg[reg_b]
+        elif op == OR:
+            self.reg[reg_a] |= self.reg[reg_b]
+        elif op == NOT:
+           
+            self.reg[reg_a] = (1 << 8) - 1 - self.reg[reg_a] 
+
+        elif op == XOR:
+            self.reg[reg_a] ^= self.reg[reg_b]
+        else:
+            raise Exception("Unsupported ALU operation")
+
+    def trace(self):
+        """
+        Handy function to print out the CPU state. You might want to call this
+        from run() if you need help debugging.
+        """
+
+        print(f"TRACE: %02X | %02X %02X %02X |" % (
+            self.pc,
+            #self.fl,
+            #self.ie,
+            self.ram_read(self.pc),
+            self.ram_read(self.pc + 1),
+            self.ram_read(self.pc + 2)
+        ), end='')
+
+        for i in range(8):
+            print(" %02X" % self.reg[i], end='')
+
+        print()
+    
+    # Operations
+
+    def hlt(self, operand_a, operand_b):
+        self.running = False
+
+    def ldi(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
+
+    def prn(self, operand_a, operand_b):
+        print(self.reg[operand_a])
+
+
+
+
+
+    def push(self, operand_a, operand_b):
+        self.sp -= 1
+        self.ram_write(self.reg[operand_a], self.sp)
+
+    def pop(self, operand_a, operand_b):
+        value = self.ram_read(self.sp)
+        self.reg[operand_a] = value
+        self.sp += 1
+
+
+
+
+    def call(self, operand_a, operand_b):
+        self.sp -= 1
+        self.ram_write(self.pc + 2, self.sp)
+        self.pc = self.reg[operand_a]
+    
+
+    def ret(self, operand_a, operand_b):
+        self.pc = self.ram_read(self.sp)
+        self.sp += 1
+    
+    def cmp(self, operand_a, operand_b):
+        self.alu(CMP, operand_a, operand_b)
+        self.pc += 3
+    
+
+
+
+    def jmp(self, operand_a, operand_b):
+        self.pc = self.reg[operand_a]
+
+    def my_jeq(self, operand_a, operand_b):
+        if self.flag == True:
+            self.pc = self.reg[operand_a]
+        else:
+            self.pc += 2
+
+    def my_jne(self, operand_a, operand_b):
+        if self.flag != 1:
+            self.pc = self.reg[operand_a]
+        else:
+            self.pc += 2
+
+
+
+    def my_and(self, operand_a, operand_b):
+        self.alu(AND, operand_a, operand_b)
+        self.pc += 3
+    def my_not(self, operand_a):
+        self.alu(NOT, operand_a)
+        self.pc += 3
+
+    def my_or(self, operand_a, operand_b):
+        self.alu(OR, operand_a, operand_b)
+        self.pc += 3
+
+    def my_xor(self, operand_a, operand_b):
+        self.alu(XOR, operand_a, operand_b)
+        self.pc += 3
+    
+        
 
     def run(self):
         """Run the CPU."""
-
-        # self.trace()
-
-
-        # print(self.mar)
-        # print(self.ram[self.pc])
-        # print(bin(int(str(self.reg[7]),10)))
-
-        self.mdr = self.ram[self.pc]
-
-        # binary_program = [i for i in self.ram]
-        # decimal_program = [int(str(i),2) for i in self.ram]
-        # print(binary_program)
-        # print(decimal_program)
-        # print(self.pc)
-
-        # dont delete this one
-        # print(bin(int(self.ram_read(self.pc),2)))
-        # print('here')
-        # print(bin(int(self.ram_read(self.pc),2)))
-        # print('here')
+        # While CPU is running
         while self.running:
+           
+            curr_instruction = self.ram_read(self.pc)
+
+          
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            #  is an ALU operation operation?
+            is_alu = (curr_instruction >> 5) & 0b00000001
+
+            if is_alu == 1:
+                self.alu(curr_instruction, operand_a, operand_b)
+           
+            else:
+                if curr_instruction in self.operations:
+                    self.operations[curr_instruction](operand_a, operand_b)
             
+            # Find next instruction 
+            next_instruction = (curr_instruction >> 4) & 0b00000001
 
-            op = ''
-         
-            # print(bin(int(self.ram_read(self.pc),2) >> 5)[-1])
-            # print('here')
-
-            if int(bin(int(self.ram_read(self.pc),2) >> 5)[-1]) == 1:
-                # print(int(bin(int(self.ram_read(self.pc),2) >> 5)[-1]))
-                # print('here')
-                # print(self.ram_read(self.pc))
-
-             
-
-                # handled by alu()
-
-                # handled_by_alu = True
-                # print(f'handled by alu')
-
-                # instruction = self.ram_read(self.pc)
+            if next_instruction == 0:
                 
-                # print(f'instruction: {bin(instruction)}')
-                # print(f'instruction >> 5: {bin(instruction>>5)}')
-
-                if self.ram_read(self.pc)[-4:] == '0000':
-                    print('0000')
-                    op = 'ADD'
-                    # instruction = self.ram_read(self.pc)
-                    operand_a = self.ram_read(self.pc+1)
-                    operand_b = self.ram_read(self.pc+2)
-                    # print(f'instruction: {bin(instruction)}')
-                    # print(f'instruction >> 5: {bin(instruction>>5)}')
-
-                    self.alu('ADD', operand_a, operand_b)
-                #print('here')
-                #print(instruction[-4:])
-
-                elif self.ram_read(self.pc)[-4:] == '0010':
-                    print('10100010')
-                    
-                    op = 'MUL'
-                    # instruction = self.ram_read(self.pc)
-                    operand_a = self.ram_read(self.pc+1)
-                    operand_b = self.ram_read(self.pc+2)
-                    #print(f'instruction: {bin(instruction)}')
-                    #print(f'instruction >> 5: {bin(instruction>>5)}')
-                    self.alu('MUL', operand_a, operand_b)
-                
-
-                elif self.ram_read(self.pc)[-4:] == '0111':
-                    print('10100111')
-                    
-                    op = 'CMP'
-                    
-                    # instruction = self.ram_read(self.pc)
-                    operand_a = self.ram_read(self.pc+1)
-                    operand_b = self.ram_read(self.pc+2)
-                    #print(f'instruction: {bin(instruction)}')
-                    #print(f'instruction >> 5: {bin(instruction>>5)}')
-                    self.alu('CMP', operand_a, operand_b)
-                    # print('here')
-
-                # if bin(instruction)[-4:] == '1000':
-                #     op = 'AND'
-
-                # instruction = self.ram_read(self.pc)
-                # type(instruction)
-            if int(bin(int(self.ram_read(self.pc),2) >> 5)[-1]) == 0:
-                # print(int(bin(int(self.ram_read(self.pc),2) >> 5)[-1]))
+                num_operands = curr_instruction >> 6
                
-                # not handled by alu()
-                # print(instruction)
-                
-                # handled_by_alu = False
+                self.pc += num_operands + 1
+            
+           
 
+            
+        
+        
 
-                #print(f'not handled by alu')
-                # instruction = bin(int(self.ram_read(self.pc),2))
-                # print(bin(int(instruction,2))[-4:])
-                if self.ram_read(self.pc)[-4:] == '0010':
-                    # print(bin(int(bin(int(self.ram_read(self.pc),2)),2))[-4:])
-                    print('10000010')
-                    op = 'LDI'
-                    operand_a = self.ram_read(self.pc+1)
-                    operand_b = self.ram_read(self.pc+2)
-
-                    self.non_alu('LDI', operand_a, operand_b)
-
-
-                    #print(self.reg[operand_a] == operand_b)
-
-                # print(bin(int(self.ram_read(self.pc),2)))
-                # instruction = bin(int(self.ram_read(self.pc),2))
-                elif self.ram_read(self.pc)[-4:] == '0101':
-                    print('01010101')
-                    
-                    op = 'JEQ'
-                    operand_a = self.ram_read(self.pc+1)
-                    self.alu('JEQ', operand_a)
-                
-
-                elif self.ram_read(self.pc)[-4:] == '0110':
-                    print('01010110')
-                    
-                    op = 'JNE'
-                    operand_a = self.ram_read(self.pc+1)
-                    self.alu('JNE', operand_a)
-
-
-
-                elif self.ram_read(self.pc)[-4:] == '0100':
-                    
-                    op = 'JMP'
-                    operand_a = self.ram_read(self.pc+1)
-                    self.alu('JMP', operand_a)
-
-                elif self.ram_read(self.pc)[-4:] == '0111':
-                    print('01000111')
-                    
-                    op = 'PRN'
-                    operand_a = self.ram_read(self.pc+1)
-                    self.non_alu('PRN', operand_a)
-                   
-
-
-                # print(instruction)
-                # print(self.ram_read(self.pc))
-
-                # instruction = bin(int(self.ram_read(self.pc),2))
-                # elif self.ram_read(self.pc)[-4:] == '0001':
-
-                elif int(self.ram_read(self.pc),2) == 1: # 0b00000001
-                
-                    # print(self.ram_read(self.pc)[-4:])  
-                    print('00000001')  
-                    op = 'HLT'
-                    operand_a = self.ram_read(self.pc+1)
-                    # print('here')
-                    # print(operand_a)
-                    # print('here')
-                    self.non_alu('HLT', operand_a)
-                    # print('HLT executed')
-                    
-                    
-
-
-
-                else:
-                    print('Error')
-                    print(int(self.ram_read(self.pc),2))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # print(instruction >> 5 & 0b1)
+   
--- ls8.py
+++ ls8.py
@@ -7,5 +7,6 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load(sys.argv[1])
+# cpu.load(sys.argv[1])
+cpu.load()
 cpu.run()
